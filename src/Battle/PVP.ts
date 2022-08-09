b907b583f259756f624644f5ebea50124e978763
debora <debora.r.serra@gmail.com>
2022-08-09 16:52:52 -0300

    feat: add req 10

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,25 @@
+import { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(
+    private player1: SimpleFighter,
+    private _player2: SimpleFighter,
+  ) {
+    super(player1);
+  }
+
+  fight(): number {
+    for (let index = 0; index < 100; index += 1) {
+      this.player.attack(this._player2);
+      if (this._player2.lifePoints <= 0) {
+        return 1;
+      }
+      this._player2.attack(this.player1);
+      if (this.player.lifePoints <= 0) {
+        return -1;
+      }
+    }
+    return 0;
+  }
+}
