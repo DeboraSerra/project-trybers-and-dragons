3ea9f1e53ab69936450fe2fe57d682db4f12d070
debora <debora.r.serra@gmail.com>
2022-08-09 16:45:47 -0300

    feat: add req 09

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -57,7 +57,7 @@ export default class Character implements Fighter {
     return this.lifePoints;
   };
 
-  public attack = (enemy: Fighter) => {
+  public attack = (enemy: SimpleFighter) => {
     const max = this.strength;
     enemy.receiveDamage(max);
   };
