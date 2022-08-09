6a5e8f1e10d61ea650812af1121aec28b1d3e5a9
debora <debora.r.serra@gmail.com>
2022-08-09 17:26:43 -0300

    feat: add req 13

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -9,13 +9,21 @@ export default class PVE extends Battle {
     super(player);
   }
 
-  fight(): number {
+  shift(ind: number): number {
     for (let index = 0; index < 100; index += 1) {
-      this.player.attack(this.monsters[0]);
-      if (this.monsters[0].lifePoints <= 0) return 1;
-      this.monsters[0].attack(this.player);
+      this.player.attack(this.monsters[ind]);
+      if (this.monsters[ind].lifePoints <= 0) return 1;
+      this.monsters[ind].attack(this.player);
       if (this.player.lifePoints <= 0) return -1;
     }
     return 0;
+  }
+
+  fight(): number {
+    for (let index = 0; index < this.monsters.length; index += 1) {
+      const shift = this.shift(index);
+      if (shift === -1) return -1;
+    }
+    return 1;
   }
 }
