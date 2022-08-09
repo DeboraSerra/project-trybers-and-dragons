12b41f606d701ddac9a30780854aceefed622e8f
debora <debora.r.serra@gmail.com>
2022-08-09 14:59:31 -0300

    feat: add req 05

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,19 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Necromancer extends Archetype {
+  private _energyType: EnergyType = 'mana';
+  private static instances = 0;
+  constructor(
+    name: string,
+  ) {
+    super(name);
+    Necromancer.instances += 1;
+  }
+
+  static createdArchetypeInstances = (): number => this.instances;
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
